6a85760509c09f1193b5b78037350349ea2bf6d1
Joe Minichino <joe.minichino@gmail.com>
2015-02-08 20:50:13 +0000

    implemented reactitive programming approach to UI

--- html/js/snaptun.js
+++ html/js/snaptun.js
@@ -6,23 +6,64 @@
     data: []
   };
 
-  function pplCollList(data) {
+  function streamMaker() {
+    var registeredListeners = [];
+    return {
+      // Have an observe function, so
+      // people who are interested can
+      // get notified when there is an update
+      observe: function (callback) {
+        registeredListeners.push(callback)
+      },
+
+      // Add a value to this stream
+      // Once added, will notify all
+      // interested parties
+      update: function (value) {
+        registeredListeners.forEach(function (cb) {
+          cb(value);
+        })
+      }
+    };
+  }
+
+  var history = streamMaker();
+
+  history.observe(pplCollList);
+
+  function pplCollList(state) {
     var list = $('#collections');
     list.empty();
-    data.forEach(function (o) {
+    state.collections.forEach(function (o) {
       list.append($('<li></li>').text(o.name + '[' + o.count + ']'));
     });
   }
 
-  function setCollectionsForState(state) {
+  function copy(obj) {
+    if (Array.isArray(obj)) {
+      return JSON.parse(JSON.stringify(obj));
+    }
+
+    var ret = {};
+    Object.keys(obj).forEach(function (prop) {
+      ret[prop] = copy(obj[prop]);
+    });
+    return ret;
+  }
+
+  function pushToStream(state) {
     return function (data) {
-      state.collections = data;
-      console.log(state.collections);
-      pplCollList(state.collections);
+      console.log('Data received, pushing to stream...');
+      var newstate = copy(state);
+      newstate.collections = data;
+      //console.log(newstate.collections);
+      //pplCollList(newstate.collections);
+      history.update(newstate);
+      return newstate;
     };
   }
 
-  var setColls = setCollectionsForState(state);
+  var setColls = pushToStream(history);
 
   function getStats() {
     $.getJSON('/listcollections', setColls);
@@ -30,5 +71,4 @@
 
   getStats();
 
-  test();
 })(window, $);
